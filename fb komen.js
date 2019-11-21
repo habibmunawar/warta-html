cf2cfb0d3034be4aef7e5d018e750823f3b35efc
habibmunawar <58038042+habibmunawar@users.noreply.github.com>
2019-11-21 21:33:49 +0700

    Add files via upload

---
+++ fb komen.js
@@ -0,0 +1,4 @@
+<div id='fb-root'/>
+<script src='//connect.facebook.net/en_GB/sdk.js#xfbml=1&amp;version=v2.7'/>
+    <div id='commentfb' style='margin: 0 -7px; width: 100%;'><fb:comments expr:href='data:post.url' num_posts='10' width='640'/></div>
+  </div>
